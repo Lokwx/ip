6a8c0808db1def16ba98e41a6ffb5a510aae4526
Wei Xiong <lokweixiong18@gmail.com>
2026-08-16 17:06:01 +0800

    Add Lokwx.java

---
+++ src/main/java/Lokwx.java
@@ -0,0 +1,16 @@
+/**
+ * Entry point for the Lokwx chatbot.
+ */
+public class Lokwx {
+    public static void main(String[] args) {
+        String banner = """
+                 _           _                   \s
+                | |         | |                  \s
+                | |     ___ | | ____      ____  __
+                | |    / _ \\| |/ /\\ \\ /\\ / /\\ \\/ /
+                | |___| (_) |   <  \\ V  V /  >  <\s
+                \\_____/\\___/|_|\\_\\  \\_/\\_/  /_/\\_\\
+                """;
+        System.out.println(banner);
+    }
+}

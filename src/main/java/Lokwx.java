7002a5d9c743f8372b9f0a4d95e5d0e2a3c4ffb1
Wei Xiong <lokweixiong18@gmail.com>
2026-08-26 10:55:07 +0800

    Update chatbox output format

--- src/main/java/Lokwx.java
+++ src/main/java/Lokwx.java
@@ -1,9 +1,12 @@
 /**
  * Entry point for the Lokwx chatbot.
  */
+
+import java.util.Scanner;
+
 public class Lokwx {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        System.out.println("_____________________________________");
         String banner = """
                  _           _                   \s
                 | |         | |                  \s
@@ -15,8 +18,12 @@ public class Lokwx {
         System.out.println(banner);
         System.out.println("Hello! I'm Lokwx.");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("______________________________________");
+
+        //Create the new scanner to read inputs
+        Scanner in = new Scanner(System.in);
+        
+        //Level-1 implementation
+        Echo.echoCommands(in);
     }
 }

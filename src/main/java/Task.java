6f8cddff683eb5168afeff961167887435abe732
Wei Xiong <lokweixiong18@gmail.com>
2026-08-26 13:24:06 +0800

    Update Task class to track task completion

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,29 @@
 public class Task {
     private String description;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
+    }
+
+    public void setDescription(String newDescription) {
+        this.description = newDescription;
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public void setIsDone(boolean newStatus) {
+        this.isDone = newStatus;
+    }
+
+    public boolean getIsDone() {
+        return this.isDone;
+    }
+
+    public String displayCheckbox() {
+        return "[" + (isDone ? "X" : " ") +  "]";
     }
 }

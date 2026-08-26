5e4fd1c72deb6209d7f9944b4bbc9a15ca467821
Wei Xiong <lokweixiong18@gmail.com>
2026-08-26 11:13:50 +0800

    Update Echo class to support listing of tasks

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -4,20 +4,36 @@ public class Echo {
 
     static final String divider = "_____________________________________";
     static final String chatbotTag = "[Lokwx]";
+
+    private static String[] inputList = new String[100];
+    private static int inputListIndex = 0;
+    
     
     public static void echoCommands(Scanner in) {
         String line;
 
         do {
             line = in.nextLine();
+
+            if (!line.equalsIgnoreCase("bye") && !line.equalsIgnoreCase("list")) {
+                inputList[inputListIndex++] = line;
+            }
+
             System.out.println(divider);
 
             //Displays the closing message if bye is inputted
-            if (line.equals("bye")) {
+            if (line.equalsIgnoreCase("bye")) {
                 System.out.println(chatbotTag + " " + "Bye. Hope to see you again soon!");
             }
+            else if (line.equalsIgnoreCase("list")) {
+             
+                //Display all the contents inside the list
+                for (int i = 0; i < inputListIndex; ++i) {
+                    System.out.println(i+1 + ". " + inputList[i]);
+                }
+            }
             else {
-                System.out.println(chatbotTag + " " + line);
+                System.out.println(chatbotTag + " " + "added: " + line);
             }
 
             System.out.println(divider);

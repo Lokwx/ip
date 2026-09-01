def3a2db212104effe7681a227b130d47d176eb3
Wei Xiong <lokweixiong18@gmail.com>
2026-09-01 17:45:11 +0800

    Add deadlineBy variable with getter and setter

--- src/main/java/lokwx/Deadline.java
+++ src/main/java/lokwx/Deadline.java
@@ -4,7 +4,18 @@ package lokwx;
  * Represents a task with a deadline.
  */
 public class Deadline extends Task {
-    public Deadline(String description) {
+    private String deadlineBy;
+
+    public Deadline(String description, String deadlineBy) {
         super(description);
+        this.deadlineBy = deadlineBy;
+    }
+
+    public String getDeadlineBy() {
+        return deadlineBy;
+    }
+
+    public void setDeadlineBy(String deadlineBy) {
+        this.deadlineBy = deadlineBy;
     }
 }

eebba86de5bae8b550033c3450dc9307de1fa2bd
Wei Xiong <lokweixiong18@gmail.com>
2026-09-01 17:45:57 +0800

    Add eventFrom and eventTo with getters and setters

--- src/main/java/lokwx/Event.java
+++ src/main/java/lokwx/Event.java
@@ -4,7 +4,27 @@ package lokwx;
  * Represents an event task occurring over a time period.
  */
 public class Event extends Task {
-    public Event(String description) {
+    private String eventFrom, eventTo;
+
+    public Event(String description, String eventFrom, String eventTo) {
         super(description);
+        this.eventFrom = eventFrom;
+        this.eventTo = eventTo;
+    }
+
+    public String getEventFrom() {
+        return eventFrom;
+    }
+
+    public void setEventFrom(String eventFrom) {
+        this.eventFrom = eventFrom;
+    }
+
+    public String getEventTo() {
+        return eventTo;
+    }
+
+    public void setEventTo(String eventTo) {
+        this.eventTo = eventTo;
     }
 }

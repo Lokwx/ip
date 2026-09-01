53cec2dc3deb8ca131eda4660c4d5a52ff992271
Wei Xiong <lokweixiong18@gmail.com>
2026-09-01 17:46:46 +0800

    Enhance InputCommandHandler to support 'todo', 'deadline', and 'event' commands

--- src/main/java/lokwx/InputCommandHandler.java
+++ src/main/java/lokwx/InputCommandHandler.java
@@ -5,6 +5,10 @@ package lokwx;
  */
 public class InputCommandHandler {
 
+    public static final String BY_DELIMITER = "/by";
+    public static final String FROM_DELIMITER = "/from";
+    public static final String TO_DELIMITER = "/to";
+
     /**
      * Processes one line of user input as a chatbot command.
      *
@@ -15,17 +19,46 @@ public class InputCommandHandler {
         String[] inputCommands = input.split(" ");
 
         switch (inputCommands[0].trim().toLowerCase()) {
-        case "bye" -> Echo.endChatbot();
-        case "list" -> taskHandler.printAllTasks();
-        case "mark" -> {
-            int itemIndex = Integer.parseInt(inputCommands[1]) - 1;
-            taskHandler.markTask(itemIndex);
-        }
-        case "unmark" -> {
-            int itemIndex = Integer.parseInt(inputCommands[1]) - 1;
-            taskHandler.unmarkTask(itemIndex);
-        }
-        default -> taskHandler.addTask(input);
+            case "bye" -> Echo.endChatbot();
+            case "list" -> taskHandler.printAllTasks();
+            case "mark" -> {
+                int itemIndex = Integer.parseInt(inputCommands[1]) - 1;
+                taskHandler.markTask(itemIndex);
+            }
+            case "unmark" -> {
+                int itemIndex = Integer.parseInt(inputCommands[1]) - 1;
+                taskHandler.unmarkTask(itemIndex);
+            }
+            case "todo" -> {
+                String description = input.substring("todo".length()).trim();
+                Todo todo = new Todo(description);
+                taskHandler.addTask(todo);
+            }
+            case "deadline" -> {
+                int byIndex = input.indexOf(BY_DELIMITER);
+
+                String description = input.substring("deadline".length(), byIndex).trim();
+                String deadlineBy = input.substring(byIndex + BY_DELIMITER.length()).trim();
+
+                Deadline deadline = new Deadline(description, deadlineBy);
+                taskHandler.addTask(deadline);
+            }
+            case "event" -> {
+                int fromIndex = input.indexOf(FROM_DELIMITER);
+                int toIndex = input.indexOf(TO_DELIMITER);
+
+                String description = input.substring("event".length(), fromIndex).trim();
+                String eventFrom = input.substring(fromIndex + FROM_DELIMITER.length(), toIndex).trim();
+                String eventTo = input.substring(toIndex + TO_DELIMITER.length()).trim();
+
+                Event event = new Event(description, eventFrom, eventTo);
+                taskHandler.addTask(event);
+            }
+            default -> {
+                String description = input;
+                Task task = new Task(input);
+                taskHandler.addTask(task);
+            }
         }
     }
 }
